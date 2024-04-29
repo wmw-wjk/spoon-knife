ad9f02f12ada6a7333bdb71e06f5106ce7511756
Michelle Tran <michelle.tran@sentry.io>
2024-04-29 13:54:09 -0400

    Add more code with fewer tests

--- func.py
+++ func.py
@@ -1,3 +1,7 @@
 def func():
     """"Test function to check if coverage is configured properly."""
     return True
+
+def another_func():
+    """See if this changes test coverage"""
+    return "hello world"
